6cd0f48bcc60f5f8451bc0bf75af6f196003d6dd
lisen <lisensir@qq.com>
2018-01-22 23:31:17 +0800

    添加超时时间配置

--- emmet-commons/src/main/java/org/lisen/emmet/commons/util/SysProp.java
+++ emmet-commons/src/main/java/org/lisen/emmet/commons/util/SysProp.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class SysProp {
 
 	private static Properties prop = null;
 
 	private static String SERVER_PORT = "zookeeper.serverports";
+	
+	private static String SESSION_TIME_OUT = "zookeeper.sessionTimeout";
 
 	static {
 		InputStream in = Object.class.getResourceAsStream("/sys.properties");
@@ -22,12 +26,20 @@ public class SysProp {
 
 	/**
 	 * 获取ZK地址及端口号
-	 * 
 	 * @return
 	 */
 	public static String getZkServerAndPort() {
 
 		return prop.getProperty(SERVER_PORT);
+	}
+	
+	/**
+	 * 获取ZK服务器会话超时时间
+	 * @return
+	 */
+	public static int getSessionTimeout() {
+		return StringUtils.isBlank(prop.getProperty(SESSION_TIME_OUT)) ? 15000
+				: Integer.parseInt(prop.getProperty(SESSION_TIME_OUT));
 	}
 
 }
